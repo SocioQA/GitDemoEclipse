4d8464d65fbc0e0c3ddc981326f273010b05a1fb
shabnam <shabnama@sociosquares.com>
2025-01-09 20:43:00 +0530

    This is my first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 91738
+ *
+ */
+module Testing {
+}
---
+++ src/test1/Login.java
@@ -0,0 +1,18 @@
+package test1;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Login {
+
+	public static void main(String[] args) {
+
+//		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		
+//		WebDriver driver= new ChromeDriver();
+//		driver.navigate().to("https://appb.myspotsaver.com");
+//		driver.manage().window().maximize();
+		System.out.println("Learning git github");
+	}
+
+}
